8b01bd17732d7ab05f4f7c33f76347a2584db2c1
kleberbaum <kleberbaum@erebos.xyz>
2019-07-30 02:14:18 +0200

    Rework production settings
    The comments and the overall structure are now a lot cleaner.

--- esite/settings/production.py
+++ esite/settings/production.py
@@ -14,4 +14,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 from .base import *
 
 
+#> Allowed hosts
+# Accept all hostnames, since we don't know in advance
+# which hostname will be used for any given Docker instance.
+# IMPORTANT: Set this to a real hostname when using this in production!
+# See https://docs.djangoproject.com/en/2.2/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
