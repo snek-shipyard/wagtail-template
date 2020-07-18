187c0f9150999c5de5c4a8b20db251a162765a23
kleberbaum <kleberbaum@erebos.xyz>
2020-07-18 12:13:01 +0200

    Update comments and formatting in settings
    The comments and formatting has been updated.
    Ref: https://github.com/snek-at/tonic

--- esite/settings/base.py
+++ esite/settings/base.py
@@ -2,131 +2,130 @@
 Django base settings for esite project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/topics/settings/
+https://docs.djangoproject.com/en/stable/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/2.2/ref/settings/
+https://docs.djangoproject.com/en/stable/ref/settings/
 """
 
 import os
 
 
-#> Root paths
+#> Root Paths
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-#> Application definition
+#> Application Definition
 # A list of strings designating all applications that are enabled in this
 # Django installation.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#installed-apps
+# See https://docs.djangoproject.com/en/stable/ref/settings/#installed-apps
 INSTALLED_APPS = [
+    # Our own pages
+    "esite.home",
     # Our own apps
-    'esite.core',
-    'esite.home',
-
-    # Third party apps
-    'corsheaders',
-    'django_filters',
-
+    "esite.core",
     # Django core apps
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
+    # Third party apps
+    "corsheaders",
+    "django_filters",
 ]
 
-#> Middleware definition
+#> Middleware Definition
 # In MIDDLEWARE, each middleware component is represented by a string: the full
 # Python path to the middleware factory’s class or function name.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#middleware
+# https://docs.djangoproject.com/en/stable/ref/settings/#middleware
+# https://docs.djangoproject.com/en/stable/topics/http/middleware/
 MIDDLEWARE = [
-    # Third party middleware
-    'corsheaders.middleware.CorsMiddleware',
-
     # Django core middleware
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    # Third party middleware
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-#> CORS origin
-# If True, the whitelist will not be used and all origins will be accepted.
-# See https://pypi.org/project/django-cors-headers/
-CORS_ORIGIN_ALLOW_ALL = True
+ROOT_URLCONF = "esite.urls"
 
-#> URL configuration
-# A string representing the full Python import path to your root URLconf.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#root-urlconf
-ROOT_URLCONF = 'esite.urls'
-
-#> Template definition
+#> Template Configuration
 # A list containing the settings for all template engines to be used with
 # Django.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#templates
+# See https://docs.djangoproject.com/en/stable/ref/settings/#templates
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(PROJECT_DIR, 'templates'),
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(PROJECT_DIR, "templates"),],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "wagtail.contrib.settings.context_processors.settings",
+                # This is a custom context processor that lets us add custom
+                # global variables to all the templates.
+                "esite.utils.context_processors.global_vars",
             ],
+            "builtins": ["pattern_library.loader_tags"],
         },
     },
 ]
 
-#> WSGI application path
+#> CORS Origin
+# If True, the whitelist will not be used and all origins will be accepted.
+# See https://pypi.org/project/django-cors-headers/
+CORS_ORIGIN_ALLOW_ALL = True
+
+#> URL Configuration
+# A string representing the full Python import path to your root URL configuration.
+# See https://docs.djangoproject.com/en/stable/ref/settings/#root-urlconf
+ROOT_URLCONF = "esite.urls"
+
+#> WSGI Application Path
 # The full Python path of the WSGI application object that Django’s built-in
 # servers (e.g. runserver) will use.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#wsgi-application
-WSGI_APPLICATION = 'esite.wsgi.application'
+# See https://docs.djangoproject.com/en/stable/ref/settings/#wsgi-application
+WSGI_APPLICATION = "esite.wsgi.application"
 
-
-#> Database definition
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+#> Database Configuration
+# This setting will use DATABASE_URL environment variable.
+# https://docs.djangoproject.com/en/stable/ref/settings/#databases
+# https://github.com/kennethreitz/dj-database-url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-#> Password validation
+#> Password Validation
 # The list of validators that are used to check the strength of passwords, see
-# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+# https://docs.djangoproject.com/en/stable/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+]
 ]
 
 #> Internationalization
-# See https://docs.djangoproject.com/en/2.2/topics/i18n/
-LANGUAGE_CODE = 'en-us'
+# https://docs.djangoproject.com/en/stable/topics/i18n/
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Europe/Vienna'
+TIME_ZONE = "Europe/Vienna"
 
 USE_I18N = True
 
@@ -134,41 +133,36 @@ USE_L10N = True
 
 USE_TZ = True
 
-#> Staticfile storage
-# ManifestStaticFilesStorage is recommended in production, to prevent outdated
-# Javascript / CSS assets being served from cache
-# (e.g. after a Wagtail upgrade).
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#staticfiles-storage
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
-#> Staticfile finder
-# Static files (CSS, JavaScript, Images)
-# See https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_FINDERS = [
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-]
-
-#> Staticfile directory
-# This setting defines the additional locations the staticfiles app will
-# traverse if the FileSystemFinder finder is enabled.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#staticfiles-dirs
+#> Staticfile Directory
+# This is where Django will look for static files outside the directories of
+# applications which are used by default.
+# https://docs.djangoproject.com/en/stable/ref/settings/#staticfiles-dirs
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
+    os.path.join(PROJECT_DIR, "static"),
 ]
 
-#> Static directory
-# The absolute path to the directory where collectstatic will collect static
-# files for deployment.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#static-root
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# This is where Django will put files collected from application directories
+# and custom direcotires set in "STATICFILES_DIRS" when
+# using "django-admin collectstatic" command.
+# https://docs.djangoproject.com/en/stable/ref/settings/#static-root
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-#> Media directory
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#media-root
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# This is the URL that will be used when serving static files, e.g.
+# https://llamasavers.com/static/
+# https://docs.djangoproject.com/en/stable/ref/settings/#static-url
+STATIC_URL = "/static/"
+
+# Where in the filesystem the media (user uploaded) content is stored.
+# MEDIA_ROOT is not used when S3 backend is set up.
+# Probably only relevant to the local development.
+# https://docs.djangoproject.com/en/stable/ref/settings/#media-root
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# The URL path that media files will be accessible at. This setting won't be
+# used if S3 backend is set up.
+# Probably only relevant to the local development.
+# https://docs.djangoproject.com/en/stable/ref/settings/#media-url
+MEDIA_URL = "/media/"
 
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2019 Werbeagentur Christian Aichner
--- esite/settings/development.py
+++ esite/settings/development.py
@@ -2,37 +2,37 @@
 Django development settings for esite project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/topics/settings/
+https://docs.djangoproject.com/en/stable/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/2.2/ref/settings/
+https://docs.djangoproject.com/en/stable/ref/settings/
 
 This development settings are unsuitable for production, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+https://docs.djangoproject.com/en/stable/howto/deployment/checklist/
 """
 
 from .base import *
 
 
-#> Debug switch
+#> Debug Switch
 # SECURITY WARNING: don't run with debug turned on in production!
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#debug
+# See https://docs.djangoproject.com/en/stable/ref/settings/#debug
 DEBUG = True
 
-#> Secret key
+#> Secret Key
 # SECURITY WARNING: keep the secret key used in production secret!
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#secret-key
-SECRET_KEY = 'ct*z11t*ns876z)!f5f3h1byn7pp1ma5i!9*oo!=dmtmnrvzcn'
+# See https://docs.djangoproject.com/en/stable/ref/settings/#secret-key
+SECRET_KEY = "ct*z11t*ns876z)!f5f3h1byn7pp1ma5i!9*oo!=dmtmnrvzcn"
 
-#> Allowed hosts
+#> Allowed Hosts
 # Accept all hostnames, since we don't know in advance.
-# See https://docs.djangoproject.com/en/2.2/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = '*'
+# See https://docs.djangoproject.com/en/stable/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = "*"
 
-#> Email backend
+#> Email Backend
 # The backend to use for sending emails.
-# See https://docs.djangoproject.com/en/2.2/topics/email/#console-backend
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# See https://docs.djangoproject.com/en/stable/topics/email/#console-backend
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # SPDX-License-Identifier: (EUPL-1.2)
 # Copyright © 2019 Werbeagentur Christian Aichner
