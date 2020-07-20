6b6dcb5dc0cdbdb8bd9bf02f961020d28af1d20f
kleberbaum <kleberbaum@erebos.xyz>
2020-07-20 13:16:57 +0200

    Rework urls for wagtail
    The urls have been adjusted to work with wagtail.

--- esite/urls.py
+++ esite/urls.py
@@ -13,11 +13,73 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.apps import apps
+from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.decorators.vary import vary_on_headers
+from django.views.generic import TemplateView
 
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+from wagtail.utils.urlpatterns import decorate_urlpatterns
+
+
+# Private URLs are not meant to be cached.
+private_urlpatterns = [
+    path('django-admin/', admin.site.urls),
+    path('admin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+]
+
+
+# Public URLs that are meant to be cached.
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r"", include(api_urls)),
+    path('sitemap.xml', sitemap),
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += [
+        # Add views for testing 404 and 500 templates
+        path('test404/', TemplateView.as_view(template_name='patterns/pages/wagtail/404.html')),
+        path('test500/', TemplateView.as_view(template_name='patterns/pages/wagtail/500.html')),
+    ]
+
+
+# Style guide
+if getattr(settings, 'PATTERN_LIBRARY_ENABLED', False) and apps.is_installed('pattern_library'):
+    private_urlpatterns += [
+        path('pattern-library/', include('pattern_library.urls')),
+    ]
+
+
+
+# Set vary header to instruct cache to serve different version on different
+# cookies, different request method (e.g. AJAX) and different protocol
+# (http vs https).
+urlpatterns = decorate_urlpatterns(
+    urlpatterns,
+    vary_on_headers('Cookie', 'X-Requested-With', 'X-Forwarded-Proto',
+                    'Accept-Encoding')
+)
+
+# Join private and public URLs.
+urlpatterns = private_urlpatterns + urlpatterns + [
+    # Add Wagtail URLs at the end.
+    # Wagtail cache-control is set on the page models's serve methods.
+    path('', include(wagtail_urls)),
 ]
 
 # SPDX-License-Identifier: (EUPL-1.2)
